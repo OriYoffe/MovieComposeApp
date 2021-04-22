dcbf0eef179225d9d494591b3b8d8cab0b840235
Ori <ori.yoffe@gmail.com>
2021-04-22 16:33:57 +0300

    last init

--- app/src/main/java/com/example/moviecomposeapp/presentation/components/BottomNavigation.kt
+++ app/src/main/java/com/example/moviecomposeapp/presentation/components/BottomNavigation.kt
@@ -8,8 +8,10 @@ import androidx.compose.material.icons.filled.AccountBalanceWallet
 import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@Preview
 @Composable
 fun BottomNavigation() {
     BottomNavigation(
--- app/src/main/java/com/example/moviecomposeapp/presentation/ui/MovieFragment.kt
+++ app/src/main/java/com/example/moviecomposeapp/presentation/ui/MovieFragment.kt
@@ -1,4 +1,33 @@
 package com.example.moviecomposeapp.presentation.ui
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Download
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
 class MovieFragment {
+
+    @Preview
+    @Composable
+    fun TestCompose() {
+        Button(onClick = { /*TODO*/ }) {
+            Column() {
+
+                Text(text = "Click Me")
+
+                Icon(
+                    imageVector = Icons.Default.Download,
+                    contentDescription = "Icon"
+                )
+
+            }
+
+        }
+    }
 }
